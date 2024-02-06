36baccbb7d1663236ba50a14e4a8a7feeaaa439c
20100215 <20100215@usc.edu.ph>
2024-02-06 09:33:44 +0800

    feat: implement shadcn ui theme provider

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <p className="text-3xl font-bold text-indigo-500">
-        This is a protected route.
+        Sab Sab Sab Sab Sab .
         <UserButton afterSignOutUrl="/"/>
       </p>
     </div>  
