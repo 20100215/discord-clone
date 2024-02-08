4052eb3f97a7413167923b6f4d61efc32d7141ba
20100215 <20100215@usc.edu.ph>
2024-02-08 15:33:49 +0800

    feat: customize your server modal Part 1

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -7,6 +7,7 @@ import {
   Dialog, 
   DialogContent, 
   DialogDescription, 
+  DialogFooter, 
   DialogHeader, 
   DialogTitle 
 } from "@/components/ui/dialog";
@@ -57,6 +58,45 @@ export const InitialModal = () => {
             You can always change it later.
           </DialogDescription>
         </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8">
+              <div className="space-y-8 px-6">
+                <div className="flex items-center justify-center text-center">
+                  TODO: Image Upload
+                </div>
+
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({field}) => (
+                    <FormItem>
+                      <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                        Server name
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          disabled={isLoading}
+                          className="bg-zinc-300/50 border-0
+                          focus-visible:ring-0 text-black 
+                          focus-visible:ring-offset-0"
+                          placeholder="Enter server name"
+                          {...field}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                >
+
+                </FormField>
+              </div>
+              <DialogFooter className="bg-gray-100 px-6 py-4">
+                <Button variant="primary" disabled={isLoading}>
+                  Create
+                </Button>
+              </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   )
