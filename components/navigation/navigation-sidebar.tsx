3ba8855980527988e3975122ea375c17d29534f6
20100215 <20100215@usc.edu.ph>
2024-02-20 14:35:33 +0800

    feat: add navigation item (list of servers)

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -3,8 +3,11 @@ import { redirect } from "next/navigation";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 
-import { NavigationAction } from "./navigation-action";
 import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+import { NavigationAction } from "./navigation-action";
+import { NavigationItem } from "./navigation-item";
 
 export const NavigationSidebar = async () => {
   const profile = await currentProfile();
@@ -26,7 +29,21 @@ export const NavigationSidebar = async () => {
     <div className="space-y-4 flex flex-col items-center
     h-full text-primary w-full dark:bg-[#1E1F22] py-3">
       <NavigationAction />
-      <Separator />
+      <Separator 
+        className="h-[2px] bg-zinc-300 dark:bg-zinc-700
+        rounded-md w-10 mx-auto"
+      />
+      <ScrollArea className="flex-1 w-full">
+        {servers.map((server)=> {
+          <div key="server.id">
+            <NavigationItem
+              id={server.id}
+              name={server.name}
+              imageUrl={server.imageUrl}
+            />
+          </div>
+        })}
+      </ScrollArea>
     </div>
   )
 }
