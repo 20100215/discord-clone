29bdcccb9c42df1c22ea212f75c96e2bda8b53e3
20100215 <20100215@usc.edu.ph>
2024-02-20 13:17:34 +0800

    feat: implement navigation sidebar

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,6 +1,7 @@
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
+import { NavigationAction } from "./navigatuon-action";
 
 export const NavigationSidebar = async () => {
   const profile = await currentProfile();
@@ -19,8 +20,9 @@ export const NavigationSidebar = async () => {
   })
 
   return (
-    <div>
-      Navigation sidebar
+    <div className="space-y-4 flex flex-col items-center
+    h-full text-primary w-full dark:bg-[#1E1F22] py-3">
+      <NavigationAction />
     </div>
   )
 }
