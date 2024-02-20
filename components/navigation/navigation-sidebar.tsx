1e33983e22f6b83c0f7dc17ea01d38463bafc6e3
20100215 <20100215@usc.edu.ph>
2024-02-20 13:04:05 +0800

    feat: add query for servers in navigation sidebar

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,4 +1,23 @@
-export const NavigationSidebar = () => {
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+export const NavigationSidebar = async () => {
+  const profile = await currentProfile();
+  if(!profile){
+    return redirect("/");
+  }
+
+  const servers = await db.server.findMany({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id
+        }
+      }
+    }
+  })
+
   return (
     <div>
       Navigation sidebar
